9ab6fa3446de48f98f5fa48108cb2e9d71f1f9db
Martin Hochstrasser <highstreeto@users.noreply.github.com>
2019-11-06 21:16:50 +0100

    Add config for vscode

--- Register-Links.ps1
+++ Register-Links.ps1
@@ -3,6 +3,7 @@
 $pwshProfile = Get-Item .\pwsh\profile.ps1
 $psGitConfig = Get-Item .\pwsh\psgit-config.psd1
 $alacrittyConfig = Get-Item .\alacritty\alacritty.yml
+$vsCodeConfig = Get-Item .\vscode\settings.json
 
 . .\ColorPrompts.ps1
 
@@ -28,4 +29,13 @@ Write-Starter -Prefix "Creating links for " -Item "PSGit" -OneLine
 # Create config dir
 New-Item -Force -Path "$env:APPDATA\powershell\PoshCode\PSGit" -ItemType Directory > $null
 New-Item -Force -Path $psGitCurrentConfig -ItemType SymbolicLink -Value ($psGitConfig.FullName) > $null
+Write-Finisher
+
+# vscode
+$vscodeCurrentConfig = "$env:APPDATA\Code\User\settings.json"
+
+Write-Starter -Prefix "Creating links for " -Item "VS Code" -OneLine
+# Create config dir
+New-Item -Force -Path "$env:APPDATA\Code\User" -ItemType Directory > $null
+New-Item -Force -Path $vscodeCurrentConfig -ItemType SymbolicLink -Value ($vsCodeConfig.FullName) > $null
 Write-Finisher
