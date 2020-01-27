8956f868416504233a42add20e74f9e88d51a8c7
Martin Hochstrasser <martin.hochstrasser@risc-software.at>
2020-01-27 09:28:53 +0100

    Remove starship from profile
    * Currently not satisfied with powershell integration (status of last command only there for external programs, \ replaced with /)

--- Register-Links.ps1
+++ Register-Links.ps1
@@ -1,8 +1,6 @@
 #Requires -RunAsAdministrator
 
 $pwshProfile = Get-Item .\pwsh\profile.ps1
-$psGitConfig = Get-Item .\pwsh\psgit-config.psd1
-$powerlineConfig = Get-Item .\pwsh\powerline-config.psd1
 $alacrittyConfig = Get-Item .\alacritty\alacritty.yml
 $vsCodeConfig = Get-Item .\vscode\settings.json
 $windowsTerminalConfig = Get-Item .\windows-terminal\profiles.json
@@ -16,39 +14,21 @@ $starshipConfig = Get-Item .\starship\starship.toml
 # pwsh
 Write-Starter -Prefix "Creating links for " -Item "pwsh" -OneLine
 New-Item -Force -Path ($profile.CurrentUserAllHosts) -ItemType SymbolicLink -Value ($pwshProfile.FullName) > $null
-Write-Finisher
+Write-Finisher -OneLine
 
 # alacritty
 Write-Starter -Prefix "Creating links for " -Item "alacritty" -OneLine
 # Create alacritty dir
 New-Item -Force -Path "$env:APPDATA\alacritty" -ItemType Directory > $null
 New-Item -Force -Path "$env:APPDATA\alacritty\alacritty.yml" -ItemType SymbolicLink -Value ($alacrittyConfig.FullName) > $null
-Write-Finisher
+Write-Finisher -OneLine
 
 # starship
 Write-Starter -Prefix "Creating links for " -Item "starship" -OneLine
 # Create starship dir
 New-Item -Force -Path "~\.config" -ItemType Directory > $null
 New-Item -Force -Path "~\.config\starship.toml" -ItemType SymbolicLink -Value ($starshipConfig.FullName) > $null
-Write-Finisher
-
-# psgit
-$psGitCurrentConfig = "$env:APPDATA\powershell\PoshCode\PSGit\Configuration.psd1"
-
-Write-Starter -Prefix "Creating links for " -Item "PSGit" -OneLine
-# Create config dir
-New-Item -Force -Path "$env:APPDATA\powershell\PoshCode\PSGit" -ItemType Directory > $null
-New-Item -Force -Path $psGitCurrentConfig -ItemType SymbolicLink -Value ($psGitConfig.FullName) > $null
-Write-Finisher
-
-# powerline
-$powerlineCurrentConfig = "$env:APPDATA\powershell\HuddledMasses.org\PowerLine\Configuration.psd1"
-
-Write-Starter -Prefix "Creating links for " -Item "PowerLine" -OneLine
-# Create config dir
-New-Item -Force -Path "$env:APPDATA\powershell\HuddledMasses.org\PowerLine" -ItemType Directory > $null
-New-Item -Force -Path $powerlineCurrentConfig -ItemType SymbolicLink -Value ($powerlineConfig.FullName) > $null
-Write-Finisher
+Write-Finisher -OneLine
 
 # vscode
 $vscodeCurrentConfig = "$env:APPDATA\Code\User\settings.json"
@@ -57,7 +37,7 @@ Write-Starter -Prefix "Creating links for " -Item "VS Code" -OneLine
 # Create config dir
 New-Item -Force -Path "$env:APPDATA\Code\User" -ItemType Directory > $null
 New-Item -Force -Path $vscodeCurrentConfig -ItemType SymbolicLink -Value ($vsCodeConfig.FullName) > $null
-Write-Finisher
+Write-Finisher -OneLine
 
 # windows-terminal
 $windowsTerminalCurrentConfig = "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json"
@@ -66,4 +46,4 @@ Write-Starter -Prefix "Creating links for " -Item "Windows Terminal" -OneLine
 # Create config dir
 New-Item -Force -Path "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" -ItemType Directory > $null
 New-Item -Force -Path $windowsTerminalCurrentConfig -ItemType SymbolicLink -Value ($windowsTerminalConfig.FullName) > $null
-Write-Finisher
+Write-Finisher -OneLine
--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -3,7 +3,6 @@
 $timer = [System.Diagnostics.Stopwatch]::StartNew()
 
 [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
-Invoke-Expression (&starship init powershell)
 
 $timer.Stop()
 "Loading of profile took {0:F2}ms" -f $timer.Elapsed.TotalMilliseconds | Write-Verbose
